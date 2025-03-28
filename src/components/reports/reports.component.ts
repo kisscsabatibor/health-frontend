8d4798ff3e0ec9ad54b2f1358016d0170a24022d
Kiss Csaba Tibor <kisscsabatibor@protonmail.com>
2025-03-28 12:58:15 +0100

    UX imporvements for report

--- src/components/reports/reports.component.ts
+++ src/components/reports/reports.component.ts
@@ -5,16 +5,21 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table'
 import { MatCardModule } from '@angular/material/card'
 import { MatIcon } from '@angular/material/icon'
 import { MatIconButton } from '@angular/material/button'
+import { filter, switchMap } from 'rxjs'
+import { MatDialog } from '@angular/material/dialog'
+import { DeleteReportConfirmationDialogComponent } from '../../dialogs/delete-report-confirmation-dialog/delete-report-confirmation-dialog.component'
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-reports',
-  imports: [MatTableModule, MatCardModule, MatIcon, MatIconButton],
+  imports: [MatTableModule, MatCardModule, MatIcon, MatIconButton, DatePipe],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss',
 })
 export class ReportsComponent {
   private reportService = inject(ReportService)
   displayedColumns: string[] = [
+    'createdAt',
     'pulse',
     'weight',
     'bloodSugar',
@@ -22,19 +27,29 @@ export class ReportsComponent {
     'actions',
   ]
   dataSource = new MatTableDataSource<ReportPayload>([])
+  readonly dialog = inject(MatDialog)
 
   ngOnInit() {
     this.reportService.getReports().subscribe((reports) => {
-      console.log(reports)
       this.dataSource.data = reports
     })
   }
 
   deleteReport(reportId: string) {
-    this.reportService.deleteReport(reportId).subscribe((value) => {
-      this.reportService.getReports().subscribe((reports) => {
+    const dialogRef = this.dialog.open(
+      DeleteReportConfirmationDialogComponent,
+      {
+        data: {},
+      },
+    )
+
+    dialogRef
+      .afterClosed()
+      .pipe(filter((res) => res))
+      .pipe(switchMap(() => this.reportService.deleteReport(reportId)))
+      .pipe(switchMap(() => this.reportService.getReports()))
+      .subscribe((reports) => {
         this.dataSource.data = reports
       })
-    })
   }
 }
---
+++ src/dialogs/delete-report-confirmation-dialog/delete-report-confirmation-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core'
+import { MatButton } from '@angular/material/button'
+import {
+  MatDialogModule,
+  MatDialogActions,
+  MatDialogRef,
+} from '@angular/material/dialog'
+import { DeleteProfileConfirmationDialogComponent } from '../delete-profile-confirmation-dialog/delete-profile-confirmation-dialog.component'
+
+@Component({
+  selector: 'app-delete-report-confirmation-dialog',
+  imports: [MatDialogModule, MatDialogActions, MatButton],
+  templateUrl: './delete-report-confirmation-dialog.component.html',
+  styleUrl: './delete-report-confirmation-dialog.component.scss',
+})
+export class DeleteReportConfirmationDialogComponent {
+  readonly dialogRef = inject(
+    MatDialogRef<DeleteProfileConfirmationDialogComponent>,
+  )
+  deleteReport() {
+    this.dialogRef.close(true)
+  }
+}
